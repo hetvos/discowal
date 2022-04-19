39f52079cdd1fa9bba13c80a92d8c69c7c8df2ef
ComfyDevs <comfydev@pm.me>
2022-04-19 20:43:59 +0200

    New index.js that is no longer broken and also removes the need for discowal.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,72 @@
-const electron = require("electron")
+const electron = require("electron");
 
-class BrowserWindow extends electron.BrowserWindow {
-    constructor(options) {
-        if (!options || !options.webPreferences || !options.webPreferences.preload || !options.title) return super(options); // eslint-disable-line constructor-super
-        super(options);
-        require("./discowal.js")(this);
-    }
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const init = async function() {
+	while (!global.mainWindowId) {
+		await sleep(50);
+	}
+
+	let win = electron.BrowserWindow.fromId(global.mainWindowId);
+	const fs = require("fs");
+	const path = require("path");
+	const { homedir } = require("os");
+	
+	const webcont = win.webContents;
+
+	const files = {
+		discocss: undefined,
+		pywalcss: undefined
+	}
+
+	webcont.on("did-finish-load", () => {
+		fs.readFile(path.resolve(homedir(), ".cache/wal/colors.css"), async (error, data) => {
+			if (error) {
+				console.error("There was an error loading pywal CSS file.");
+				console.error("Here's the error:", error);
+			} else {
+				files.pywalcss = await webcont.insertCSS(data.toString()).catch((error) => {
+					console.error("Failed to insert pywal CSS file.");
+					console.error("Here's the error:", error);
+				});
+
+				fs.watch(path.resolve(homedir(), ".cache/wal/colors.css"), () => {
+					fs.readFile(path.resolve(homedir(), ".cache/wal/colors.css"), async (error, data) => {
+						if (error) {
+							console.error("There was an error loading pywal CSS file.");
+							console.error("Here's the error:", error);
+						} else {
+							webcont.removeInsertedCSS(files.pywalcss).catch((error) => {
+								console.error("Failed to uninsert pywal CSS file.");
+								console.error("Here's the error:", error);
+							});
+
+							files.pywalcss = await webcont.insertCSS(data.toString()).catch((error) => {
+								console.error("Failed to reinsert pywal CSS file.");
+								console.error("Here's the error:", error);
+							});
+				await webcont.executeJavaScript("doMagic()")
+						}
+					});
+				})
+			}
+		fs.readFile(path.resolve(__dirname, "magic.js"), async (error, data) => {
+			await webcont.executeJavaScript(data.toString())
+})
+		});
+
+		fs.readFile(path.resolve(__dirname, "disco.css"), async (error, data) => {
+			if (error) {
+				console.error("There was an error loading discowal CSS file.");
+				console.error("Here's the error:", error);
+			} else {
+				files.discocss = await webcont.insertCSS(data.toString()).catch((error) => {
+					console.error("Failed to insert discowal CSS file.");
+					console.error("Here's the error:", error);
+				});
+			}
+		});
+	});
 }
 
-Object.assign(BrowserWindow, electron.BrowserWindow);
-
-const newElectron = new Proxy(electron, {
-    get: function(target, prop) {
-        if (prop === "BrowserWindow") return BrowserWindow;
-        return target[prop];
-    }
-});
-const electronPath = require.resolve("electron");
-delete require.cache[electronPath].exports;
-require.cache[electronPath].exports = newElectron;
+init()
